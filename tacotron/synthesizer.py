6f80fe2d51fb1c3daa326ed02ab2e85482134056
dresson <edresson1@gmail.com>
2019-03-12 13:07:12 -0300

    update

--- tacotron/synthesizer.py
+++ tacotron/synthesizer.py
@@ -12,36 +12,34 @@ from tacotron.models import create_model
 from tacotron.utils import plot
 from tacotron.utils.text import text_to_sequence
 
-
 class Synthesizer:
 	def load(self, checkpoint_path, hparams, gta=False, model_name='Tacotron'):
-                with tf.device('/cpu:0'): 
-                        log('Constructing model: %s' % model_name)
-                        inputs = tf.placeholder(tf.int32, [None, None], 'inputs')
-                        input_lengths = tf.placeholder(tf.int32, [None], 'input_lengths')
-                        with tf.variable_scope('model') as scope:
-                                self.model = create_model(model_name, hparams)
-                                self.model.initialize(inputs, input_lengths)
-                                self.alignments = self.model.alignments
-                                self.lf0_outputs = self.model.lf0_outputs
-                                self.mgc_outputs = self.model.mgc_outputs
-                                self.bap_outputs = self.model.bap_outputs
+                log('Constructing model: %s' % model_name)
+                inputs = tf.placeholder(tf.int32, [None, None], 'inputs')
+                input_lengths = tf.placeholder(tf.int32, [None], 'input_lengths')
+                with tf.variable_scope('model') as scope:
+                        self.model = create_model(model_name, hparams)
+                        self.model.initialize(inputs, input_lengths)
+                        self.alignments = self.model.alignments
+                        self.lf0_outputs = self.model.lf0_outputs
+                        self.mgc_outputs = self.model.mgc_outputs
+                        self.bap_outputs = self.model.bap_outputs
 
-                        self.gta = gta
-                        self._hparams = hparams
-                        #pad input sequences with the <pad_token> 0 ( _ )
-                        self._pad = 0
+                self.gta = gta
+                self._hparams = hparams
+                #pad input sequences with the <pad_token> 0 ( _ )
+                self._pad = 0
 
-                        log('Loading checkpoint: %s' % checkpoint_path)
-                        #Memory allocation on the GPU as needed
-                        config = tf.ConfigProto()
-                        config.gpu_options.allow_growth = True
+                log('Loading checkpoint: %s' % checkpoint_path)
+                #Memory allocation on the GPU as needed
+                config = tf.ConfigProto()
+                config.gpu_options.allow_growth = True
 
-                        self.session = tf.Session(config=config)
-                        self.session.run(tf.global_variables_initializer())
+                self.session = tf.Session(config=config)
+                self.session.run(tf.global_variables_initializer())
 
-                        saver = tf.train.Saver()
-                        saver.restore(self.session, checkpoint_path)
+                saver = tf.train.Saver()
+                saver.restore(self.session, checkpoint_path)
 
 
 	def synthesize(self, texts, basenames, out_dir, log_dir):
@@ -54,8 +52,8 @@ class Synthesizer:
 			self.model.inputs: seqs,
 			self.model.input_lengths: np.asarray(input_lengths, dtype=np.int32),
 		}
-		with tf.device('/cpu:0'):                        
-			lf0s, mgcs, baps, alignments = self.session.run([self.lf0_outputs, self.mgc_outputs, self.bap_outputs, self.alignments], feed_dict=feed_dict)
+		                        
+		lf0s, mgcs, baps, alignments = self.session.run([self.lf0_outputs, self.mgc_outputs, self.bap_outputs, self.alignments], feed_dict=feed_dict)
 
 		for i, _ in enumerate(lf0s):
 			# Write the predicted features to disk
