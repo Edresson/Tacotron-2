39863573283294388d46cd1b535caa906bba725f
dresson <edresson1@gmail.com>
2019-03-26 20:21:54 -0300

    update

--- hparams.py
+++ hparams.py
@@ -18,8 +18,8 @@ hparams = tf.contrib.training.HParams(
 	rescale = True, #Whether to rescale audio prior to preprocessing
 	rescaling_max = 0.999, #Rescaling value
 	trim_silence = True, #Whether to clip silence in Audio (at beginning and end of audio only, not the middle)
-	clip_mels_length = True, #For cases of OOM (Not really recommended, working on a workaround)
-	max_mel_frames = 2000,  #Only relevant when clip_mels_length = True
+	clip_mels_length = False, #For cases of OOM (Not really recommended, working on a workaround)
+	max_mel_frames = 740,  #Only relevant when clip_mels_length = True
 
 	# Use LWS (https://github.com/Jonathan-LeRoux/lws) for STFT and phase reconstruction
 	# It's preferred to set True to use with https://github.com/r9y9/wavenet_vocoder
